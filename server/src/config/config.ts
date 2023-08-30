b8543fb6a9038349feb40f602cc164e8314ef329
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-08-30 10:05:35 +0200

    basic config on server side 30.08.23 10:05

---
+++ server/src/config/config.ts
@@ -0,0 +1,11 @@
+const config = {
+    mongo: {
+        url: `mongodb+srv://Lukasz:mypassword>@cluster0.as4jqw4.mongodb.net/?retryWrites=true&w=majority`
+    },
+
+    server: {
+        host: 'localhost',
+        port: 1337
+    }
+};
+export default config;
