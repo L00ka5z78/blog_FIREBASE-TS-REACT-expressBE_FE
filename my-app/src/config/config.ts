df5856b5386a8645a79dcc52c27fd6cb5446a4ac
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-08-30 09:40:46 +0200

    firebae config and custom loggin file added 30.08.23 09:40

---
+++ my-app/src/config/config.ts
@@ -0,0 +1,15 @@
+const config = {
+    firebase: {
+        apiKey: 'AIzaSyC8IMXfPcmF4Z9_JFIjpKWztUKC-mlf7cg',
+        authDomain: 'blog-29849.firebaseapp.com',
+        projectId: 'blog-29849',
+        storageBucket: 'blog-29849.appspot.com',
+        messagingSenderId: '772380061288',
+        appId: '1:772380061288:web:e7742c4698df1469e4d5dd'
+    },
+
+    server: {
+        url: 'http://localhost:1337'
+    }
+};
+export default config;
