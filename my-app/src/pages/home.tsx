9d8ba11b08998547c67db231f2b445d5fd54f1fc
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-08-31 11:29:38 +0200

    basic client side created. 31.08.23 11:29

--- my-app/src/pages/home.tsx
+++ my-app/src/pages/home.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { IPageProps } from '../interfaces';
+import { Navigation } from '../components/Navigation/navbar';
+import { Header } from '../components/Header/header';
+import { Container } from 'reactstrap';
 
 export const HomePage = (props: IPageProps) => {
-    return <p>Home Page</p>;
+    return (
+        <Container fluid className="p-0">
+            <Navigation />
+            <Header title="An old school climber webside" headline="Check out what other climbers have to say about!" />
+            <Container className="mt-5">Blog stuff, discussion about grades and gear for sure....</Container>
+        </Container>
+    );
 };
