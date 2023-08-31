a0ecc7feb779524f7865e3da9862039896f6b679
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-08-31 09:57:29 +0200

    routes configuration created 31.08.23 09Ø57

---
+++ my-app/src/config/routes.ts
@@ -0,0 +1,10 @@
+import { IRoute } from '../interfaces';
+
+const authRoutes: IRoute = [];
+
+const blogRoutes: IRoute = [];
+
+const mainRoutes: IRoute = [];
+
+const routes: IRoute = [...authRoutes, ...blogRoutes, ...mainRoutes];
+export default routes;
