495562f3c9a33aef430eddef4237bca1646dd26c
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-09-01 10:40:23 +0200

    firebase.ts completed 01.09.23 10:40

--- my-app/src/config/firebase.ts
+++ my-app/src/config/firebase.ts
@@ -1,8 +1,15 @@
 /** integrating firebase configuration */
 
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 import config from './config';
 
 const Firebase = firebase.initializeApp(config.firebase);
+
+export const Providers = {
+    google: new firebase.auth.GoogleAuthProvider()
+};
+
+export const auth = firebase.auth();
+export default Firebase;
