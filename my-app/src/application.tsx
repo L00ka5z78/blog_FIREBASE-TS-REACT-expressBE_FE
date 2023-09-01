488250eda876caa9feb5e084891d0895bbe437e2
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-09-01 11:21:30 +0200

    useState, useReducer and temp debugging feat added 01.09.23 11:21

--- my-app/src/application.tsx
+++ my-app/src/application.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import routes from './config/routes';
 import { RouteChildrenProps } from 'react-router-dom';
+import { initialUserState, userReducer } from './context/user';
 
 export interface IApplicationProps {}
 
 export const Application = (props: IApplicationProps) => {
+    const [userState, userDispatch] = useReducer(userReducer, initialUserState);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    /** for debugging purposes */
+    const [authState, setAuthState] = useState<string>('Checking localstorage ... ...');
+
+    useEffect(() => {
+        setTimeout(() => {}, 1000);
+    }, []);
+
     return (
         <Switch>
             {routes.map((route, index) => {
