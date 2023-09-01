0fa69605a7b7e0550d1ff214c5624bc35210d189
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-09-01 11:53:34 +0200

    loadin component added 01.09.23 11:54

--- my-app/src/components/Loading/LoadingComponent.tsx
+++ my-app/src/components/Loading/LoadingComponent.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Card, CardBody } from 'reactstrap';
 
 export interface ILoadingProps {
     dotType?: string;
     children?: any;
 }
+
 export const Loading = (props: ILoadingProps) => {
     const { children, dotType } = props;
 
@@ -16,6 +18,32 @@ export const Loading = (props: ILoadingProps) => {
         </div>
     );
 };
+
 Loading.defaultProps = {
+    dotType: 'dot-bricks'
+};
+
+export interface ILoadingComponentProps {
+    card?: boolean;
+    dotType?: string;
+    children?: any;
+}
+export const LoadingComponent = (props: ILoadingComponentProps) => {
+    const { card, children, dotType } = props;
+
+    if (card) {
+        return (
+            <Card>
+                <CardBody>
+                    <Loading dotType={dotType}>{children}</Loading>
+                </CardBody>
+            </Card>
+        );
+    }
+    return <Loading dotType={dotType}>{children}</Loading>;
+};
+
+LoadingComponent.defaultProps = {
+    card: true,
     dotType: 'dot-bricks'
 };
