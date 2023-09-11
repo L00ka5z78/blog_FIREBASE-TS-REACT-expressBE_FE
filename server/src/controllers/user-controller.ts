09b6241b856d737eee926441fed65b46ca0d0a3c
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-09-11 07:55:06 +0200

    user.controller in progress 11..09.23 07:55

---
+++ server/src/controllers/user-controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const validate = (req: Request, res: Response, next: NextFunction) => {};
+
+export const create = (req: Request, res: Response, next: NextFunction) => {};
+
+export const login = (req: Request, res: Response, next: NextFunction) => {};
+
+export const read = (req: Request, res: Response, next: NextFunction) => {};
+
+export const readAll = (req: Request, res: Response, next: NextFunction) => {};
