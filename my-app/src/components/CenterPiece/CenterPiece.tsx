918f91743fa93eb2cc70db551dbb988fb65f0cd9
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-09-11 07:36:50 +0200

    user interface and model added 11.09.23 07:36

--- my-app/src/components/CenterPiece/CenterPiece.tsx
+++ my-app/src/components/CenterPiece/CenterPiece.tsx
@@ -1,11 +1,39 @@
+// import React from 'react';
+// import { Container } from 'reactstrap';
+
+// export interface ICenterPieceProps {
+//     children?: any;
+// }
+
+// export const CenterPiece = (props: ICenterPieceProps) => {
+//     const { children } = props;
+
+//     return (
+//         <Container fluid className="p-0">
+//             <Container
+//                 style={{
+//                     position: 'absolute',
+//                     left: '50%',
+//                     top: '50%',
+//                     transform: 'translate(-50%, -50%',
+//                     WebkitTransform: 'translate(-50%, -50%'
+//                 }}
+//                 className="d-flex justify-content-center"
+//             >
+//                 {children}
+//             </Container>
+//         </Container>
+//     );
+// };
+
 import React from 'react';
 import { Container } from 'reactstrap';
 
-export interface ICenterPieceProps {
-    children?: any;
+export interface Props {
+    children?: any; //add
 }
 
-export const CenterPiece = (props: ICenterPieceProps) => {
+export const CenterPiece = (props: Props) => {
     const { children } = props;
 
     return (
@@ -15,8 +43,8 @@ export const CenterPiece = (props: ICenterPieceProps) => {
                     position: 'absolute',
                     left: '50%',
                     top: '50%',
-                    transform: 'translate(-50%, -50%',
-                    WebkitTransform: 'translate(-50%, -50%'
+                    transform: 'translate(-50%, -50%)',
+                    WebkitTransform: 'translate(-50%, -50%)'
                 }}
                 className="d-flex justify-content-center"
             >
