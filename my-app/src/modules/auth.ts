280f9bf7cc96617e65d4612c1e8ca62fe4446e3b
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-09-11 09:42:59 +0200

    FE module/auth authenticate function created 11.09.23 09:42

--- my-app/src/modules/auth.ts
+++ my-app/src/modules/auth.ts
@@ -1,5 +1,9 @@
 import firebase from 'firebase/compat';
 import { auth } from '../config/firebase';
+import { IUser } from '../interfaces';
+import axios from 'axios';
+import config from '../config/config';
+import logging from '../config/loging';
 
 export const SignInWithSocialMedia = (provider: firebase.auth.AuthProvider) =>
     new Promise<firebase.auth.UserCredential>((resolve, reject) => {
@@ -7,3 +11,27 @@ export const SignInWithSocialMedia = (provider: firebase.auth.AuthProvider) =>
             .then((result) => resolve(result))
             .catch((error) => reject(error));
     });
+
+export const Authenticate = async (uid: string, name: string, fire_token: string, callback: (error: string | null, user: IUser | null) => void) => {
+    try {
+        const response = await axios({
+            method: 'POST',
+            url: `${config.server.url}/users/login`,
+            data: {
+                uid,
+                name
+            },
+            headers: { Authorization: `Bearer ${fire_token}` }
+        });
+        if (response.status === 200 || response.status === 201 || response.status === 304) {
+            logging.info('Succesfully authenticated.');
+            callback(null, response.data.user);
+        } else {
+            logging.warn('Unable to authenticate');
+            callback('Unable to authenticate', null);
+        }
+    } catch (error) {
+        logging.error(error);
+        callback('Unable to authenticate', null);
+    }
+};
