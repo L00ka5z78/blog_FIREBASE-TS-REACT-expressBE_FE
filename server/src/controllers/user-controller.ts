2693da2b1ab48f3219c33da6208998d07d6d6a11
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-09-11 08:13:31 +0200

    validate function added 11.09.23 08:13

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -1,6 +1,29 @@
 import { Request, Response, NextFunction } from 'express';
+import logging from '../config/loging';
+import User from '../models/user.model';
 
-export const validate = (req: Request, res: Response, next: NextFunction) => {};
+export const validate = (req: Request, res: Response, next: NextFunction) => {
+    logging.info('Token validated, returning user...');
+    let firebase = res.locals.firebase;
+
+    return User.findOne({ uid: firebase.uid })
+        .then((user) => {
+            if (user) {
+                return res.status(200).json({ user });
+            } else {
+                return res.status(401).json({
+                    message: 'user not found'
+                });
+            }
+        })
+        .catch((error) => {
+            logging.error(error);
+            return res.status(500).json({
+                message: error.message,
+                error
+            });
+        });
+};
 
 export const create = (req: Request, res: Response, next: NextFunction) => {};
 
