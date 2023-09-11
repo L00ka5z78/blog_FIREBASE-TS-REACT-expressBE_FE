e339aabedfdbf841eedc1ca1328218d7e518f1a5
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-09-11 08:34:13 +0200

    create user method added 11.09.23 08:34

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import logging from '../config/loging';
 import User from '../models/user.model';
+import mongoose from 'mongoose';
 
 export const validate = (req: Request, res: Response, next: NextFunction) => {
     logging.info('Token validated, returning user...');
@@ -25,7 +26,29 @@ export const validate = (req: Request, res: Response, next: NextFunction) => {
         });
 };
 
-export const create = (req: Request, res: Response, next: NextFunction) => {};
+export const create = (req: Request, res: Response, next: NextFunction) => {
+    logging.info('Attempting to register user...');
+    let { uid, name } = req.body;
+    let fire_token = res.locals.fire_token;
+
+    const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        uid,
+        name
+    });
+    return user
+        .save()
+        .then((newUser) => {
+            logging.info(`New user ${uid} created ...`);
+            return res.status(201).json({ user: newUser, fire_token });
+        })
+        .catch((error) => {
+            logging.error(error);
+            return res.status(500).json({
+                error
+            });
+        });
+};
 
 export const login = (req: Request, res: Response, next: NextFunction) => {};
 
