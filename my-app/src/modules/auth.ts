ea15e8904fc191204234d836aa6d052a029af9b0
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-09-11 09:47:02 +0200

    FE validate functio built 11.09.23 09:46

--- my-app/src/modules/auth.ts
+++ my-app/src/modules/auth.ts
@@ -35,3 +35,24 @@ export const Authenticate = async (uid: string, name: string, fire_token: string
         callback('Unable to authenticate', null);
     }
 };
+
+export const Validate = async (fire_token: string, callback: (error: string | null, user: IUser | null) => void) => {
+    try {
+        const response = await axios({
+            method: 'GET',
+            url: `${config.server.url}/users/login`,
+            headers: { Authorization: `Bearer ${fire_token}` }
+        });
+
+        if (response.status === 200 || response.status === 304) {
+            logging.info('Succesfully validated.');
+            callback(null, response.data.user);
+        } else {
+            logging.warn('Unable to validate');
+            callback('Unable to validate', null);
+        }
+    } catch (error) {
+        logging.error(error);
+        callback('Unable to validate', null);
+    }
+};
