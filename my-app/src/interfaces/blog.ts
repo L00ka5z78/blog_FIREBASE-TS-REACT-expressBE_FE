ffdaabca0dca27479513b0437a57038853961c5b
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-09-12 14:05:49 +0200

    FE blog interface added 12.09.23 14:05

---
+++ my-app/src/interfaces/blog.ts
@@ -0,0 +1,12 @@
+import { IUser } from './user';
+
+export interface IBlog {
+    _id: string;
+    title: string;
+    author: IUser | string;
+    content: string;
+    headline: string;
+    picture?: string;
+    createdAt: string;
+    updateAt: string;
+}
