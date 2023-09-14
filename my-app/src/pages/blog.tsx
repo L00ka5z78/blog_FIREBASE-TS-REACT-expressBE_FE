1ef7fdbb4f06e53a8dd74ed77f2aff2edcca0baf
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-09-14 14:18:28 +0200

    modal created 14.09.23 14:18

--- my-app/src/pages/blog.tsx
+++ my-app/src/pages/blog.tsx
@@ -1,8 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../context/user';
-import { useHistory } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import axios from 'axios';
 import config from '../config/config';
+import { Loading, LoadingComponent } from '../components/Loading/LoadingComponent';
+import { Button, Container, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import { Navigation } from '../components/Navigation/navbar';
+import { ErrorText } from '../components/Error/ErrorText';
 
 export const BlogPage = (props: any) => {
     const [_id, setId] = useState<string>('');
@@ -38,7 +42,7 @@ export const BlogPage = (props: any) => {
                 method: 'GET',
                 url: `${config.server.url}/blogs/read/${_id}`
             });
-            if (response.status === 200 || response.status === 304) {
+            if (response.status === 201 || response.status === 304) {
                 setBlog(response.data.blog);
             } else {
                 setError(`Unable to retrieve blog ${_id}`);
@@ -51,22 +55,47 @@ export const BlogPage = (props: any) => {
     };
 
     const deleteBlog = async () => {
+        setDeleting(true);
         try {
             const response = await axios({
-                method: 'GET',
-                url: `${config.server.url}/blogs/read/${_id}`
+                method: 'DELETE',
+                url: `${config.server.url}/blogs/${_id}`
             });
-            if (response.status === 200 || response.status === 304) {
-                setBlog(response.data.blog);
+            if (response.status === 200) {
+                history.push('/');
             } else {
                 setError(`Unable to retrieve blog ${_id}`);
+                setDeleting(false);
             }
         } catch (error: any) {
             setError(error.message);
-        } finally {
-            setLoading(false);
+            setDeleting(false);
         }
     };
+    if (loading) return <LoadingComponent>Loading Blog ... ...</LoadingComponent>;
 
-    return <p>Blog Page</p>;
+    if (blog) {
+        return (
+            <Container fluid className="p-0">
+                <Navigation />
+                <Modal isOpen={modal}>
+                    <ModalHeader>Delete</ModalHeader>
+                    <ModalBody>
+                        {deleting ? <Loading /> : 'Are you sure you want to delete this blog??'}
+                        <ErrorText error={error} />
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="danger" onClick={() => deleteBlog()}>
+                            Delete Permanently
+                        </Button>
+                        <Button color="secondary" onClick={() => setModal(false)}>
+                            Cancel
+                        </Button>
+                    </ModalFooter>
+                </Modal>
+            </Container>
+        );
+    } else {
+        return <Redirect to="/" />;
+    }
 };
