bdbd671b8f45393aae143746585f43740b71b595
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-09-14 14:03:06 +0200

    getblog function created 14.09.23 14:02

--- my-app/src/pages/blog.tsx
+++ my-app/src/pages/blog.tsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../context/user';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import config from '../config/config';
 
 export const BlogPage = (props: any) => {
     const [_id, setId] = useState<string>('');
@@ -18,9 +20,53 @@ export const BlogPage = (props: any) => {
         let blogID = props.match.params.blogID;
 
         if (blogID) {
+            setId(blogID);
         } else {
+            history.push('/');
         }
+        // eslint-disable-next-line
     }, []);
+
+    useEffect(() => {
+        if (_id !== '') getBlog();
+        // eslint-disable-next-line
+    }, [_id]);
+
+    const getBlog = async () => {
+        try {
+            const response = await axios({
+                method: 'GET',
+                url: `${config.server.url}/blogs/read/${_id}`
+            });
+            if (response.status === 200 || response.status === 304) {
+                setBlog(response.data.blog);
+            } else {
+                setError(`Unable to retrieve blog ${_id}`);
+            }
+        } catch (error: any) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const deleteBlog = async () => {
+        try {
+            const response = await axios({
+                method: 'GET',
+                url: `${config.server.url}/blogs/read/${_id}`
+            });
+            if (response.status === 200 || response.status === 304) {
+                setBlog(response.data.blog);
+            } else {
+                setError(`Unable to retrieve blog ${_id}`);
+            }
+        } catch (error: any) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return <p>Blog Page</p>;
 };
