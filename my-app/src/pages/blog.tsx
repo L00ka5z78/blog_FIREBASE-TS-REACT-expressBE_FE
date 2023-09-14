bb7075c764bfb71ea11d7c5a1c30bc20e96baa5d
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-09-14 13:55:30 +0200

    blog page in progress 14.09.23 13:55

--- my-app/src/pages/blog.tsx
+++ my-app/src/pages/blog.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
-import { IPageProps } from '../interfaces';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../context/user';
+import { useHistory } from 'react-router-dom';
 
-export const BlogPage = (props: IPageProps) => {
+export const BlogPage = (props: any) => {
+    const [_id, setId] = useState<string>('');
+    const [blog, setBlog] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string>('');
+
+    const [modal, setModal] = useState<boolean>(false);
+    const [deleting, setDeleting] = useState<boolean>(false);
+
+    const { user } = useContext(UserContext).userState;
+    const history = useHistory();
+
+    useEffect(() => {
+        let blogID = props.match.params.blogID;
+
+        if (blogID) {
+        } else {
+        }
+    }, []);
+
     return <p>Blog Page</p>;
 };
